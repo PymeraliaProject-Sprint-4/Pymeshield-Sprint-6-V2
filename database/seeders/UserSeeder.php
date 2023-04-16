7ed1fc44bfb5061bbd7590db13af8e8ad32220b2
Fran Zahinos <franciscozahinos@iesmontsia.org>
2023-04-16 13:27:45 +0200

    crear 3 usuarios por defecto

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Str;
 
 class UserSeeder extends Seeder
 {
@@ -15,7 +16,54 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        User::factory(50)->create()->each(function ($user){
+
+        $client = User::create([
+            'name' => 'Client',
+            'last_name' => 'Pymeshield',
+            'email' => 'client@pymeshield.com',
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'nick_name' => 'client',
+            'phone' => fake()->phoneNumber(),
+            'type' => 'client',
+            'remember_token' => Str::random(10),
+            'company_id' => 1,
+        ]);
+
+        $client->assignRole('client');
+
+        $worker = User::create([
+            'name' => 'Worker',
+            'last_name' => 'Pymeshield',
+            'email' => 'worker@pymeshield.com',
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'nick_name' => 'worker',
+            'phone' => fake()->phoneNumber(),
+            'type' => 'worker',
+            'remember_token' => Str::random(10),
+            'company_id' => 1,
+        ]);
+
+        $worker->assignRole('worker');
+
+        $admin = User::create([
+            'name' => 'Admin',
+            'last_name' => 'Pymeshield',
+            'email' => 'admin@pymeshield.com',
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'nick_name' => 'admin',
+            'phone' => fake()->phoneNumber(),
+            'type' => 'admin',
+            'remember_token' => Str::random(10),
+            'company_id' => 1,
+        ]);
+
+        $admin->assignRole('admin');
+
+        User::factory(50)->create()->each(function ($user) {
+
             switch ($user->type) {
                 case 'admin':
                     $user->assignRole('admin');
