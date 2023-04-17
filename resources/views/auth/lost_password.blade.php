8dcf517c9f72027faf57643661716fc3e8e2e796
Tud0r420 <tudoreusebiuconstantin@iesmontsia.org>
2023-04-17 16:51:26 +0200

    validacions login

--- resources/views/auth/lost_password.blade.php
+++ resources/views/auth/lost_password.blade.php
@@ -44,7 +44,10 @@
                         <input id="email-address" name="email" type="email" value="{{ old('email') }}"
                             autocomplete="email" required
                             class="relative block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-orange-500 focus:outline-none focus:ring-orange-500 sm:text-sm"
-                            placeholder="{{__("email")}}">
+                            placeholder="{{__("email")}}"
+                            onblur="validateEmail()">
+
+                        <div id="email-error" class="text-sm text-red-600 pt-2"></div>
                     </div>
                 </div>
 
@@ -77,4 +80,28 @@
             </form>
         </div>
     </div>
+
+    <script>
+        function validateEmail() {
+            const emailInput = document.getElementById("email-address");
+            const emailError = document.getElementById("email-error");
+        
+            // Expresión regular para validar correo electrónico
+            const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        
+            if (!emailRegex.test(emailInput.value)) {
+                // Si el correo electrónico no es válido, muestra un mensaje de error
+                emailError.textContent = "{{__("validation.email")}}";
+                emailError.classList.remove("hidden");
+                emailInput.classList.add("border-red-500");
+                emailInput.classList.remove("border-green-500");
+            } else {
+                // Si el correo electrónico es válido, oculta el mensaje de error
+                emailError.classList.add("hidden");
+                emailInput.classList.remove("border-red-500");
+                emailInput.classList.add("border-green-500");
+            }
+        }
+    </script>
+        
 @stop
