d9a8f688a48a6fde3cfe6d907fd401164c8d7c0b
Tud0r420 <94829026+Tud0r420@users.noreply.github.com>
2023-04-18 18:41:02 +0200

    jquery inecesari

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,4 @@
 
-import 'jquery/dist/jquery.min.js'
 //defineAsync para cargar los componentes de forma asíncrona
 import { createApp, defineAsyncComponent } from "vue/dist/vue.esm-bundler";
 import laravelPermissionToVuejs from 'laravel-permission-to-vuejs';
